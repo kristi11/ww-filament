c8c7d60d67bdc2cbfa22e3cfe2a88a727b7e5581
kristi <tanellari@gmail.com>
2024-08-05 01:56:33 -0400

    wip

--- resources/views/livewire/public/guest-nav-links.blade.php
+++ resources/views/livewire/public/guest-nav-links.blade.php
@@ -41,14 +41,12 @@
                         <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
                            href="#gallery">Gallery</a>
                     </li>
+                    <li class="mr-3">
+                        <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+                           href="#cta">Contact</a>
+                    </li>
                 </ul>
-                <a
-                    href="#cta"
-                    id="navAction"
-                    class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
-                >
-                    Contact
-                </a>
+
             </div>
         </div>
         <hr class="border-b border-gray-100 opacity-25 my-0 py-0"/>
