0fb747737c8b829848c4f66aeab77d35e6462a83
kristi <tanellari@gmail.com>
2024-08-10 02:24:28 -0400

    wip

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -133,16 +133,16 @@ class UserResource extends Resource
             //            'edit' => Pages\EditUser::route('/{record}/edit'),
         ];
     }
-//    public static function canEdit(Model $record): bool
-//    {
-//        return false;
-//    }
-//    public static function canDelete(Model $record): bool
-//    {
-//        return false;
-//    }
-//    public static function canCreate(): bool
-//    {
-//        return false;
-//    }
+    public static function canEdit(Model $record): bool
+    {
+        return false;
+    }
+    public static function canDelete(Model $record): bool
+    {
+        return false;
+    }
+    public static function canCreate(): bool
+    {
+        return false;
+    }
 }
