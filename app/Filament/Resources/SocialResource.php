7d351314e27f7d4a9ddbb2ba734b71026ad29fcb
kristi <tanellari@gmail.com>
2024-08-18 02:36:27 -0400

    wip

--- app/Filament/Resources/SocialResource.php
+++ app/Filament/Resources/SocialResource.php
@@ -142,13 +142,13 @@ class SocialResource extends Resource
         return ! $recordExists;
     }
 
-//    public static function canDelete(Model $record): bool
-//    {
-//        return false;
-//    }
-//
-//    public static function canEdit(Model $record): bool
-//    {
-//        return false;
-//    }
+    public static function canDelete(Model $record): bool
+    {
+        return false;
+    }
+
+    public static function canEdit(Model $record): bool
+    {
+        return false;
+    }
 }
