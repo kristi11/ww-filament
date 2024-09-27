d0352ffa1324ffe38ba719701eba837abd200858
kristi <tanellari@gmail.com>
2024-09-27 12:13:25 -0400

    wip

---
+++ resources/views/components/get-repo.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <section id="login" class="bg-white border-b py-8 lg:p-20">
+        <div
+            class="bg-gray-50 container flex flex-wrap justify-center mx-auto pb-4 pt-4 rounded-lg shadow-lg">
+            <h2 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+                Get the <a class="text-indigo-600 hover:text-indigo-800" href="https://github.com/kristi11/ww-filament" target="_blank">Code</a>
+            </h2>
+        </div>
+    </section>
+</div>
--- resources/views/livewire/public/guest_content.blade.php
+++ resources/views/livewire/public/guest_content.blade.php
@@ -1,6 +1,8 @@
 <div>
     <body class="leading-normal tracking-normal text-white gradient"
           style="font-family: 'Source Sans Pro', sans-serif;">
+    {{--The <x-get-repo/> is only for developers to get a link to the github repository. It's safe to delete this component when using this repository for your own project. This is just a blade file with a link in it and has not links to other places in the project--}}
+    <x-get-repo/>
     <livewire:display-socials/>
     <livewire:guest-login/>
     <livewire:display-guest-services/>
