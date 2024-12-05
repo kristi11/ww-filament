2985290a8bcfcceefb43fce2bb230f26c07ee8cd
kristi <tanellari@gmail.com>
2024-12-04 23:47:11 -0500

    wip

--- app/Filament/Resources/HeroResource.php
+++ app/Filament/Resources/HeroResource.php
@@ -312,10 +312,10 @@ class HeroResource extends Resource
         return !$recordExists;
     }
 
-    public static function canEdit(Model $record): bool
-    {
-        return false;
-    }
+//    public static function canEdit(Model $record): bool
+//    {
+//        return false;
+//    }
 
     public static function canDelete(Model $record): bool
     {
