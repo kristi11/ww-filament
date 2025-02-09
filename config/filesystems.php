708379db56ebdaba8a49154c15782b2f95071df6
kristi <tanellari@gmail.com>
2025-02-09 13:43:35 -0500

    wip

--- config/filesystems.php
+++ config/filesystems.php
@@ -100,6 +100,7 @@ return [
             'secret' => env('DO_SPACES_SECRET'),
             'region' => env('DO_SPACES_REGION'),
             'bucket' => env('DO_SPACES_BUCKET'),
+            'prefix' => '',
             'url' => env('DO_SPACES_URL'),
             'endpoint' => env('DO_SPACES_ENDPOINT'),
             'use_path_style_endpoint' => env('DO_SPACES_USE_PATH_STYLE_ENDPOINT', false),
